893ecfdaa6180873e3f02cac507210c03064786a
KM_CM <send.nex@gmail.com>
2025-05-01 12:57:02 +0300

    Update __init__.py

--- KM_CM_MacrosButNot/__init__.py
+++ KM_CM_MacrosButNot/__init__.py
@@ -5,6 +5,9 @@ __MACROS__: dict[ str, _Any ] = {}
 def DEFINE( Name: str, Value: _Any = True ) -> None:
     __MACROS__[ Name ] = Value
 
+def UNDEF( Name: str ) -> None:
+    __MACROS__.pop( Name, None )
+
 def IFDEF( Name: str ) -> bool:
     try:
         __MACROS__[ Name ]
